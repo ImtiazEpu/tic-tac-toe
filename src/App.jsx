5deff300491f2a0c3b0810b3e757e8c6049bcd74
ImtiazEpu <imtiazepu@programmer.net>
2025-05-14 16:37:50 +0600

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+export default function Board() {
+    return (
+        <>
+            <div>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+            </div>
+            <div>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+            </div>
+            <div>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
+                </button>
+            </div>
+        </>
+    );
+}
