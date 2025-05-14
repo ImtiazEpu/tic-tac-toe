a31150a65e0482bad1f5ec491cd101b644edb578
ImtiazEpu <imtiazepu@programmer.net>
2025-05-14 19:20:10 +0600

    Add turn-based functionality to the Board component
    Introduced `xIsNext` state to alternate turns between 'X' and 'O'. Updated the `handleClick` function to manage turn logic and toggle players accordingly. Minor formatting fixes were also applied to some arrow functions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 
-function Square( {value, onSquareClick} ) {
+function Square({value, onSquareClick}) {
 
     return (
         <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg"
@@ -11,28 +11,37 @@ function Square( {value, onSquareClick} ) {
 
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
 
     function handleClick(i) {
         const newSquares = squares.slice();
-        newSquares[i] = 'X';
+        if (xIsNext) {
+            newSquares[i] = 'X';
+            setXIsNext(false);
+        } else {
+            newSquares[i] = 'O';
+            setXIsNext(true);
+        }
         setSquares(newSquares);
+        setXIsNext(!xIsNext);
     }
+
     return (
         <>
             <div className="flex justify-center">
-                <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
             </div>
             <div className={"flex justify-center"}>
-                <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
             </div>
             <div className={"flex justify-center"}>
-                <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
             </div>
         </>
     );
