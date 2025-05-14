bb5366e2ef881e8d2f4f897bf228797d5406c06d
ImtiazEpu <imtiazepu@programmer.net>
2025-05-14 17:31:07 +0600

    Refactor Board to include interactive Square components.
    Replaced static buttons with a reusable Square component using state to handle clicks. This improves code reusability and introduces interactivity by updating the button value on click.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
+import {useState} from 'react';
+
+function Square() {
+
+    const [value, setValue] = useState(null);
+    function handleClick() {
+       setValue('X')
+    }
+
+    return (
+        <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg" onClick={handleClick}>{value}
+        </button>
+    );
+}
+
 export default function Board() {
     return (
         <>
-            <div>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
+            <div className="flex justify-center">
+                <Square />
+                <Square />
+                <Square />
             </div>
-            <div>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
+            <div className={"flex justify-center"}>
+                <Square />
+                <Square />
+                <Square />
             </div>
-            <div>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
-                <button className="bg-white border border-gray-400 rounded-xl h-12 w-12 m-1 leading-9 text-lg">x
-                </button>
+            <div className={"flex justify-center"}>
+                <Square />
+                <Square />
+                <Square />
             </div>
         </>
     );
